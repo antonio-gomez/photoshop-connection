7409569467b5b96eba8428b1f11fbb25fa31ce55
Antonio Gomez <hello@antoniogomez.me>
2016-11-27 20:23:16 -0800

    update(): basic project scaffolding and including vendor libraries

---
+++ src/photoshop-connection.js
@@ -0,0 +1,14 @@
+/*
+ *
+ *  Photoshop-Connection
+ *  Node.js module for connecting to Adobe Photoshop Server.
+ *  Author: Antonio Gomez (https://github.com/antonio-gomez)
+ * 
+ *
+ *  @link https://github.com/antonio-gomez/photoshop-connection
+ *  @version v0.0.0
+ *  @license MIT
+ *
+ */
+
+'use strict'
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: `${__dirname}/src/photoshop-connection.js`,
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'photoshop-connection.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel', query: { presets: ['es2015'] } }
+        ]
+    },
+    // Ignoring libraries that are not intended for
+    // in-browser usage: https://github.com/webpack/react-starter/issues/3
+    node: {
+        net: 'empty'
+    }
+}
